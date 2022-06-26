8598b59e4c97f916380baef3758101ac4d77a6f7
Lucas Santos <lucas.leossan@gmail.com>
2022-06-25 21:03:05 -0300

    Second Step Github Clone for TroubleShoot Trailhead

--- force-app/main/default/lwc/controls/controls.js
+++ force-app/main/default/lwc/controls/controls.js
@@ -1,25 +1,17 @@
-/**
- * @description       :
- * @author            : Lucas Santos - Accenture
- * @group             :
- * @last modified on  : 06-23-2022
- * @last modified by  : Lucas Santos - Accenture
-**/
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Controls extends LightningElement {
-    factors = [0,2,3,4,5,6];
+  @api operand = 1;
 
-    handleAdd() {
-        this.dispatchEvent(new CustomEvent('add'));
-      }
-      handleSubtract() {
-        this.dispatchEvent(new CustomEvent('subtract'));
-      }
-      handleMultiply(event) {
-        const factor = event.target.dataset.factor;
-        this.dispatchEvent(new CustomEvent('multiply', {
-          detail: factor
-        }));
-      }
+  handleAdd() {
+    this.dispatchEvent(new CustomEvent('add', {
+      detail: this.operand
+    }));
+  }
+  
+  handleSubtract() {
+    this.dispatchEvent(new CustomEvent('subtract', {
+      detail: this.operand
+    }));
+  }
 }
